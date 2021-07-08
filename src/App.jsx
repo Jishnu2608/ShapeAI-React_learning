413d91a056d158d59037132465b9f7ba4eb4dca6
Jishnu2608 <jishnudeepborah@gmail.com>
2021-07-08 10:14:12 +0530

    States and props

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import User from "./components/user";
+function App() {
+  return (
+  <div>
+    <h1>Users</h1>
+    <User name="Jishnu" description="FSWD"/>
+    <User name="Reon" description="FSWD"/>
+    <User name="Nivedita" description="FSWD"/>
+  </div>
+  );
+}
+
+export default App;
+
+//props - A process that is being passed from one component to another.
+
+//state - A set of data that an individual component holds.
